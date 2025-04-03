684a82fba2bea5bacd711d3e7f56ca082c2b3922
hugo-b-fx <gucci60610@gmail.com>
2025-04-03 06:45:15 +0200

    dd

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string') {
+      return '';
+    }
+    return Array.from(set)
+      .filter((values) => values.startsWith(startString) && typeof values === 'string')
+      .map((values) => values.slice(startString.length)).join('-');
+  }
